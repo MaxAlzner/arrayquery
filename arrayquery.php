f20da40621ed9150ee872e10eb78f3ba1a4cf9e6
MaxAlzner <maxalzner@gmail.com>
2015-10-27 19:10:16 +0000

    working queries

--- arrayquery.php
+++ arrayquery.php
@@ -2,17 +2,345 @@
 
 class ArrayQuery
 {
-    protected $stack = null;
+    public $stack = null;
     
     function __construct(array $stack)
     {
-        $this->stack = $stack;
+        $this->stack = array_values($stack);
     }
     
     function __toString()
     {
-        return json_encode($this->stack, JSON_PRETTY_PRINT);
+        return json_encode($this->stack);
     }
+    
+    function toArray()
+    {
+        return $this->stack;
+    }
+    
+    function all($predicate = null)
+    {
+        $query = true;
+        $callback = $this->validate($predicate);
+        foreach($this->stack as $item)
+        {
+            if ($callback !== false)
+            {
+                $query = $query && boolval(call_user_func($callback, $item));
+            }
+            else if (is_bool($item))
+            {
+                $query = $query && $item;
+            }
+        }
+        
+        return $query;
+    }
+    
+    function any($predicate = null)
+    {
+        $callback = $this->validate($predicate);
+        foreach($this->stack as $item)
+        {
+            if ($callback !== false && boolval(call_user_func($callback, $item)))
+            {
+                return true;
+            }
+            else if (is_bool($item) && $item === true)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    function average($selector = null)
+    {
+        $total = 0;
+        $count = 0;
+        $callback = $this->validate($selector);
+        foreach($this->stack as $item)
+        {
+            $query = $callback !== false ? call_user_func($callback, $item) : $item;
+            if (is_int($query) || is_float($query))
+            {
+                $total += $query;
+                $count++;
+            }
+        }
+        
+        return $count !== 0 ? ($total / $count) : 0;
+    }
+    
+    function contains($value)
+    {
+        foreach($this->stack as $item)
+        {
+            if ($item === $value)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    function concat(self $other)
+    {
+        return new self(array_merge($this->stack, $other->stack));
+    }
+    
+    function count($predicate = null)
+    {
+        $count = 0;
+        $callback = $this->validate($predicate);
+        foreach($this->stack as $item)
+        {
+            $count += $callback !== false ? (call_user_func($callback, $item) !== true ? 0 : 1) : 1;
+        }
+        
+        return $count;
+    }
+    
+    function except(self $other)
+    {
+        return new self(array_merge(array_diff($this->stack, $other->stack), array_diff($other->stack, $this->stack)));
+    }
+    
+    function first()
+    {
+        return count($this->stack) > 0 ? $this->stack[0] : null;
+    }
+    
+    function groupby($selector)
+    {
+        $query = new self([]);
+        $callback = $this->validate($selector);
+        if ($callback !== false)
+        {
+            $groups = [];
+            foreach ($this->stack as $item)
+            {
+                $key = call_user_func($callback, $item);
+                $key = is_scalar($key) ? $key : 0;
+                $groups[$key][] = $item;
+            }
+            
+            ksort($groups);
+            foreach ($groups as $group)
+            {
+                foreach ($group as $item)
+                {
+                    $query->stack[] = $item;
+                }
+            }
+        }
+        
+        return $query;
+    }
+    
+    function intersect(self $other)
+    {
+        return new self(array_intersect($this->stack, $other->stack));
+    }
+    
+    function join(self $inner, $outerSelector, $innerSelector, $selector)
+    {
+        $query = new self([]);
+        
+        return $query;
+    }
+    
+    function last()
+    {
+        $count = count($this->stack);
+        return $count > 0 ? $this->stack[$count - 1] : null;
+    }
+    
+    function max($selector = null)
+    {
+        $max = null;
+        $callback = $this->validate($selector);
+        foreach ($this->stack as $item)
+        {
+            $query = $callback !== false ? call_user_func($callback, $item) : $item;
+            if (is_scalar($query))
+            {
+                $max = $max === null || $max < $query ? $query : $max;
+            }
+        }
+        
+        return $max;
+    }
+    
+    function min($selector = null)
+    {
+        $min = null;
+        $callback = $this->validate($selector);
+        foreach ($this->stack as $item)
+        {
+            $query = $callback !== false ? call_user_func($callback, $item) : $item;
+            if (is_scalar($query))
+            {
+                $min = $min === null || $min > $query ? $query : $min;
+            }
+        }
+        
+        return $min;
+    }
+    
+    function orderby($selector)
+    {
+        $query = new self([]);
+        $callback = $this->validate($selector);
+        if ($callback !== false)
+        {
+            $ordered = [];
+            foreach($this->stack as $item)
+            {
+                $key = call_user_func($callback, $item);
+                if (isset($key))
+                {
+                    $ordered[$key] = $item;
+                }
+            }
+            
+            foreach ($ordered as $item)
+            {
+                $query->stack[] = $item;
+            }
+        }
+        
+        return $query;
+    }
+    
+    function orderbydesc($selector)
+    {
+        return new self(array_reverse($this->orderby($selector)->stack));
+    }
+    
+    function reverse()
+    {
+        return new self(array_reverse($this->stack));
+    }
+    
+    function select($selector)
+    {
+        $query = new self([]);
+        $callback = $this->validate($selector);
+        if ($callback !== false)
+        {
+            foreach($this->stack as $item)
+            {
+                $result = call_user_func($callback, $item);
+                if (isset($result))
+                {
+                    $query->stack[] = $result;
+                }
+            }
+        }
+        
+        return $query;
+    }
+    
+    function single($predicate = null)
+    {
+        $query = $predicate !== null ? $this->where($predicate) : new self($this->stack);
+        return count($query->stack) === 1 ? $query->stack[0] : null;
+    }
+    
+    function skip($count, $predicate = null)
+    {
+        $query = $predicate !== null ? $this->where($predicate) : new self($this->stack);
+        return new self(array_slice($query->stack, intval($count)));
+    }
+    
+    function sum($selector = null)
+    {
+        $sum = 0;
+        $callback = $this->validate($selector);
+        foreach ($this->stack as $item)
+        {
+            $result = $callback !== false ? call_user_func($callback, $item) : $item;
+            if (is_int($result) || is_float($result))
+            {
+                $sum += $result;
+            }
+        }
+        
+        return $sum;
+    }
+    
+    function take($start, $predicate = null)
+    {
+        $query = $predicate !== null ? $this->where($predicate) : new self($this->stack);
+        return new self(array_slice($query->stack, 0, intval($start)));
+    }
+    
+    function where($predicate)
+    {
+        $query = new self([]);
+        $callback = $this->validate($predicate);
+        if ($callback !== false)
+        {
+            foreach($this->stack as $item)
+            {
+                if (call_user_func($callback, $item) === true)
+                {
+                    $query->stack[] = $item;
+                }
+            }
+        }
+        
+        return $query;
+    }
+    
+    function each($statement)
+    {
+        $callback = $this->validate($statement);
+        if ($callback !== false)
+        {
+            foreach($this->stack as $item)
+            {
+                call_user_func($callback, $item);
+            }
+        }
+    }
+    
+    protected function validate($statement)
+    {
+        if (is_callable($statement))
+        {
+            return $statement;
+        }
+        else if (is_string($statement))
+        {
+            $segments = explode('=>', $statement);
+            if (count($segments) === 2)
+            {
+                $arg = trim($segments[0]);
+                $body = trim($segments[1]);
+                try
+                {
+                    eval('$callback = function (' . $arg . ') {return ' . $body . ';};');
+                    return $callback;
+                }
+                catch (Exception $e)
+                {
+                    throw new Exception('Statement is invalid: ' . $statement, 0, $e);
+                }
+            }
+        }
+        
+        return false;
+    }
+}
+
+function array_query($stack)
+{
+    return new ArrayQuery($stack);
 }
 
 ?>
---
+++ test/index.php
@@ -0,0 +1,129 @@
+<?php
+
+include '../arrayquery.php';
+
+header('Content-Type: text/plain');
+
+echo 'all:' . PHP_EOL;
+echo json_encode(array_query([false, true, false])->all()) . PHP_EOL;
+echo json_encode(array_query([true, true])->all()) . PHP_EOL;
+echo json_encode(array_query([1, 2, 3])->all('$i => ($i % 2) === 1')) . PHP_EOL;
+echo json_encode(array_query([1, 2, 3])->all('$i => $i > 0')) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'any:' . PHP_EOL;
+echo json_encode(array_query([false, true, false])->any()) . PHP_EOL;
+echo json_encode(array_query([false, false])->any()) . PHP_EOL;
+echo json_encode(array_query([1, 2, 3])->any('$i => ($i % 2) === 1')) . PHP_EOL;
+echo json_encode(array_query([1, 2, 3])->any('$i => $i > 4')) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'average:' . PHP_EOL;
+echo array_query([1, 2, 3])->average() . PHP_EOL;
+echo array_query([-1, 2, 3])->average() . PHP_EOL;
+echo array_query([['index' => -1], ['index' => 3], ['index' => 4]])->average('$i => $i[\'index\']') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->average() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'contains:' . PHP_EOL;
+echo json_encode(array_query([1, 2])->contains(2)) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'concat:' . PHP_EOL;
+echo array_query([1, 2])->concat(array_query([3, 4])) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'count:' . PHP_EOL;
+echo array_query([1, 2, 3])->count() . PHP_EOL;
+echo array_query([1, 2, 3])->count('$i => $i >= 2') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'except:' . PHP_EOL;
+echo array_query([1, 2])->except(array_query([2, 3])) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'first:' . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->first() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'groupby:' . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->groupby('$i => $i') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->groupby('$str => $str[1]') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'intersect:' . PHP_EOL;
+echo array_query([1, 2])->intersect(array_query([2, 3])) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'join:' . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->join(
+    array_query(['house', 'garage', 'yard']),
+    '$str => $str',
+    '$str => $str',
+    '$str0, $str1 => [\'animal\' => $str0, \'area\' => $str1]') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'last:' . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->last() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'max:' . PHP_EOL;
+echo array_query([1, 2, 1, 3, 4, 3])->max() . PHP_EOL;
+echo array_query([['index' => 1], ['index' => 2], ['index' => 4]])->max('$i => $i[\'index\']') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->max() . PHP_EOL;
+echo array_query([0])->max() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'min:' . PHP_EOL;
+echo array_query([1, 2, 1, 3, 4, 3])->min() . PHP_EOL;
+echo array_query([['index' => 1], ['index' => 2], ['index' => 4]])->min('$i => $i[\'index\']') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->min() . PHP_EOL;
+echo array_query([0])->min() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'orderby:' . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->orderby('$i => $i') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->orderby('$str => $str[strlen($str) - 1]') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'orderbydesc:' . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->orderbydesc('$i => $i') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->orderbydesc('$str => $str[strlen($str) - 1]') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'reverse:' . PHP_EOL;
+echo array_query([1, 2, 3])->reverse() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'select:' . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->select('$str => strtoupper($str)[0]') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'single:' . PHP_EOL;
+echo array_query([1])->single() . PHP_EOL;
+echo array_query([1, 2, 3])->single('$i => $i === 2') . PHP_EOL;
+echo json_encode(array_query([1, 2, 3])->single('$i => $i === \'cat\'')) . PHP_EOL;
+echo PHP_EOL;
+
+echo 'skip:' . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->skip(3) . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->skip(3, '$i => $i > 1') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'sum:' . PHP_EOL;
+echo array_query([1, 2, 1, 3, 4, 3])->sum() . PHP_EOL;
+echo array_query([['index' => 1], ['index' => 2], ['index' => 4]])->sum('$i => $i[\'index\']') . PHP_EOL;
+echo array_query(['dog', 'rabbit', 'cat', 'fish'])->sum() . PHP_EOL;
+echo PHP_EOL;
+
+echo 'take:' . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->take(3) . PHP_EOL;
+echo array_query([1, 2, 1, 1, 3, 4, 3])->take(3, '$i => $i > 1') . PHP_EOL;
+echo PHP_EOL;
+
+echo 'where:' . PHP_EOL;
+echo array_query([1, 2, 3, 4])->where(function ($i) { return ($i % 2) === 1; }) . PHP_EOL;
+echo array_query([1, 2, 3, 4])->where('$i => ($i % 2) === 1') . PHP_EOL;
+echo PHP_EOL;
+
+?>
